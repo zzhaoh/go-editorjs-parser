0748c91a14452836c1b27959a043cef46540ad1d
Rodrigo Corrêa <rodrigocosta@ext.worten.pt>
2021-10-18 17:11:57 +0100

    Update docs

--- example/example.go
+++ example/example.go
@@ -2,39 +2,40 @@ package main
 
 import (
 	"gitlab.com/rodrigoodhin/go-editorjs-parser/pkg/html"
+	"gitlab.com/rodrigoodhin/go-editorjs-parser/pkg/markdown"
 	"log"
 )
 
 func main() {
 
-	//err := html.Sample("example/example.json", "example/sample.html")
-	//if err != nil {
-	//	log.Println("It was not possible to parse json to html file\n",err)
-	//	return
-	//}
-
-	//err := html.Bootstrap("example/example.json", "example/bootstrap.html")
-	//if err != nil {
-	//	log.Println("It was not possible to parse json to html file\n",err)
-	//	return
-	//}
-
-	err := html.Bulma("example/example.json", "example/bulma.html")
+	err := html.Sample("example/example.json", "example/sample.html")
 	if err != nil {
 		log.Println("It was not possible to parse json to html file\n",err)
 		return
 	}
 
-	//err := html.Custom("example/example.json", "example/custom.html", "example/test.json")
-	//if err != nil {
-	//	log.Println("It was not possible to parse json to html file\n",err)
-	//	return
-	//}
+	err = html.Bootstrap("example/example.json", "example/bootstrap.html")
+	if err != nil {
+		log.Println("It was not possible to parse json to html file\n",err)
+		return
+	}
 
-	//err := markdown.Parser("./example/example.json", "example")
-	//if err != nil {
-	//	log.Println("It was not possible to parse json to markdown file\n",err)
-	//	return
-	//}
+	err = html.Bulma("example/example.json", "example/bulma.html")
+	if err != nil {
+		log.Println("It was not possible to parse json to html file\n",err)
+		return
+	}
+
+	err = html.Custom("example/example.json", "example/custom.html", "example/test.json")
+	if err != nil {
+		log.Println("It was not possible to parse json to html file\n",err)
+		return
+	}
+
+	err = markdown.Parser("./example/example.json", "example")
+	if err != nil {
+		log.Println("It was not possible to parse json to markdown file\n",err)
+		return
+	}
 
 }
